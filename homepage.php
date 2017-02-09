fe0faeeadc11e244882f38d81ecc50d3b78a2116
Rafgros <raf.gros@gmail.com>
2017-02-08 20:28:04 -0800

    Created a homepage in html with a php tag

---
+++ homepage.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>PHP tag in html</title>
+    </head>
+ 
+    <body>
+	
+	 <?php include("menu.php"); ?>
+	 
+	 <div id="corps">
+		<h1>CONTENT</h1>
+	 </div>
+	
+	</body>
+</html>
